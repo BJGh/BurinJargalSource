d9965537297a86a5aa614fe92c294d11e3843d64
BJ_Gh <49895341+BJGh@users.noreply.github.com>
2026-08-07 08:56:25 +0800

    Update hostgamemenu.cpp

--- app/legion/hostgamemenu.cpp
+++ app/legion/hostgamemenu.cpp
@@ -1,201 +1,182 @@
-//========= Copyright Valve Corporation, All rights reserved. ============//
-//
-// Purpose: The menu that allows a player to start a game  
-//
-// $Revision: $
-// $NoKeywords: $
-//===========================================================================//
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-#include "menumanager.h"
-#include "basemenu.h"
-#include "vgui_controls/listpanel.h"
-#include "vgui_controls/textentry.h"
-#include "vgui_controls/Button.h"
-#include "tier1/KeyValues.h"
-#include "networkmanager.h"
-#include "legion.h"
-#include "inetworkmessagelistener.h"
-#include "networkmessages.h"
-#include "tier2/tier2.h"
-
-
-//-----------------------------------------------------------------------------
-// Constructor, destructor
-//-----------------------------------------------------------------------------
-class CHostGameMenu : public CBaseMenu, public INetworkMessageListener
+namespace sourcesharp.app.legion
 {
-	DECLARE_CLASS_SIMPLE( CHostGameMenu, CBaseMenu );
+    public class CHostGameMenu : CBaseMenu, INetworkMessageListener
+    {
+        public CHostGameMenu(GameObject hostGameMenuGO) : base(hostGameMenuGO)
+        {
+            m_pPlayerList = hostGameMenuGO.transform.Find("PlayerList").GetComponent<ListPanel>();
+            m_pServerIP = hostGameMenuGO.transform.Find("ServerIP").GetComponent<TextEntry>();
+            m_pServerName = hostGameMenuGO.transform.Find("ServerName").GetComponent<TextEntry>();
+            m_pChatLog = hostGameMenuGO.transform.Find("ChatLog").GetComponent<TextEntry>();
+            m_pChatEntry = hostGameMenuGO.transform.Find("ChatEntry").GetComponent<TextEntry>();
+            m_pPlayerName = hostGameMenuGO.transform.Find("PlayerName").GetComponent<TextEntry>();
+            m_pStartGame = hostGameMenuGO.transform.Find("StartGame").GetComponent<Button>();
 
-public:
-	CHostGameMenu( vgui::Panel *pParent, const char *pPanelName );
-	virtual ~CHostGameMenu();
+            m_pStartGame.onClick.AddListener(OnStartGameClick);
+        }
 
-	// Called when a particular network message occurs
-	virtual void OnNetworkMessage( NetworkMessageRoute_t route, INetworkMessage *pNetworkMessage );
-	virtual void OnCommand( const char *pCommand );
+        public void OnNetworkMessage(NetworkMessageRoute_t route, INetworkMessage pNetworkMessage)
+        {
+            // Handle network messages
+        }
 
-	MESSAGE_FUNC( OnTextNewLine, "TextNewLine" );
+        public void OnCommand(string pCommand)
+        {
+            // Handle commands
+        }
 
-private:
-	vgui::ListPanel *m_pPlayerList;
-	vgui::TextEntry *m_pServerIP;
-	vgui::TextEntry *m_pServerName;
-	vgui::TextEntry *m_pChatLog;
-	vgui::TextEntry *m_pChatEntry;
-	vgui::TextEntry *m_pPlayerName;
-	vgui::Button *m_pStartGame;
-};
+        public void OnStartGameClick()
+        {
+            // Handle start game button click
+        }
+
+        public void OnTextNewLine()
+        {
+            // Handle new line of text
+        }
+
+        private ListPanel m_pPlayerList;
+        private TextEntry m_pServerIP;
+        private TextEntry m_pServerName;
+        private TextEntry m_pChatLog;
+        private TextEntry m_pChatEntry;
+        private TextEntry m_pPlayerName;
+        private Button m_pStartGame;
+        public class HostGameMenu : Panel
+        {
+            private ListPanel m_playerList;
+            private TextEntry m_serverIP;
+            private TextEntry m_serverName;
+            private TextEntry m_playerName;
+            private TextEntry m_chatLog;
+            private TextEntry m_chatEntry;
+            private Button m_startGame;
+
+            public HostGameMenu(Panel pParent, string pPanelName) : base(pParent, pPanelName)
+            {
+                m_playerList = new ListPanel(this, "PlayerList");
+                m_playerList.AddColumnHeader(0, "color", "Color", 52, 0);
+                m_playerList.AddColumnHeader(1, "player", "Player Name", 128, 0);
+                m_playerList.SetSelectIndividualCells(false);
+                m_playerList.SetEmptyListText("No Players");
+                m_playerList.SetDragEnabled(false);
+                m_playerList.AddActionSignalTarget(this);
+                m_playerList.SetSortFunc(0, PlayerNameSortFunc);
+                m_playerList.SetSortFunc(1, PlayerNameSortFunc);
+                m_playerList.SetSortColumn(1);
+
+                m_serverIP = new TextEntry(this, "ServerIP");
+                m_serverName = new TextEntry(this, "ServerName");
+
+                m_playerName = new TextEntry(this, "PlayerName");
+                m_playerName.SetMultiline(false);
+
+                m_chatLog = new TextEntry(this, "ChatLog");
+                m_chatLog.SetMultiline(true);
+                m_chatLog.SetVerticalScrollbar(true);
+
+                m_chatEntry = new TextEntry(this, "ChatEntry");
+                m_chatEntry.AddActionSignalTarget(this);
+                m_chatEntry.SetMultiline(false);
+                m_chatEntry.SendNewLine(true);
+
+                m_startGame = new Button(this, "StartGame", "Start Game", this);
+
+                // Load control settings from a file
+                // The equivalent of this will depend on your specific project layout
+                LoadControlSettingsFromFile("resource/hostgamemenu.res");
+
+                m_playerName.SetText("Unnamed");
+
+                if (!NetworkManager.Instance.HostGame())
+                {
+                    m_startGame.SetEnabled(false);
+                    return;
+                }
+
+                m_serverIP.SetText(NetworkSystem.Instance.GetLocalAddress());
+                m_serverName.SetText(NetworkSystem.Instance.GetLocalHostName());
+
+                g_pNetworkManager.RemoveListener(NETWORK_MESSAGE_SERVER_TO_CLIENT, LEGION_NETMESSAGE_GROUP, CHAT_MESSAGE, this);
+                g_pNetworkManager.RemoveListener(NETWORK_MESSAGE_CLIENT_TO_SERVER, LEGION_NETMESSAGE_GROUP, CHAT_MESSAGE, this);
+            }
+            ~CHostGameMenu()
+            {
+                g_pNetworkManager.RemoveListener(NETWORK_MESSAGE_SERVER_TO_CLIENT, LEGION_NETMESSAGE_GROUP, CHAT_MESSAGE, this);
+                g_pNetworkManager.RemoveListener(NETWORK_MESSAGE_CLIENT_TO_SERVER, LEGION_NETMESSAGE_GROUP, CHAT_MESSAGE, this);
+            }
+            void OnNetworkMessage(NetworkMessageRoute_t route, INetworkMessage pNetworkMessage)
+            {
+                if (route == NETWORK_MESSAGE_SERVER_TO_CLIENT)
+                {
+                    CNetworkMessage_Chat pChatMsg = (CNetworkMessage_Chat)pNetworkMessage;
+                    m_pChatLog.InsertString(pChatMsg.m_Message.Get());
+                    m_pChatLog.InsertChar('\n');
+                }
+                else
+                {
+                    // If this message was received from an client, broadcast it to all other clients
+                    g_pNetworkManager.BroadcastServerToClientMessage(pNetworkMessage);
+                }
+            }
+            void OnTextNewLine()
+            {
+                CNetworkMessage_Chat msg = new CNetworkMessage_Chat();
+
+                int nLen = m_pChatEntry.GetTextLength();
+                if (nLen > 0)
+                {
+                    char[] pText = new char[nLen + 1];
+                    m_pChatEntry.GetText(pText, nLen + 1);
+                    m_pChatEntry.SetText("");
+
+                    int nLenName = m_pPlayerName.GetTextLength();
+                    char[] pName = new char[nLenName + 8];
+                    if (nLenName == 0)
+                    {
+                        nLenName = 7;
+                        Q_strcpy(pName, "unnamed");
+                    }
+                    else
+                    {
+                        m_pPlayerName.GetText(pName, nLenName + 1);
+                    }
+
+                    int nTotalLen = nLen + nLenName;
+
+                    char[] message = new char[nTotalLen + 3];
+                    Q_snprintf(message, nTotalLen + 3, $"[{pName}] {pText}");
+                    msg.m_Message = new string(message);
+
+                    g_pNetworkManager.PostClientToServerMessage(msg);
+                }
+            }
+            void OnCommand(string pCommand)
+            {
+                if (string.Equals(pCommand, "CancelHostGame", StringComparison.OrdinalIgnoreCase))
+                {
+                    g_pNetworkManager.StopHostingGame();
+                    g_pMenuManager.PopMenu();
+                    return;
+                }
+
+                if (string.Equals(pCommand, "StartGame", StringComparison.OrdinalIgnoreCase))
+                {
+                    IGameManager.StartNewLevel();
+                    g_pMenuManager.PopAllMenus();
+                    return;
+                }
+
+                BaseClass.OnCommand(pCommand);
+            }
 
 
-//-----------------------------------------------------------------------------
-// Hooks the menu into the menu system
-//-----------------------------------------------------------------------------
-REGISTER_MENU( "HostGameMenu", CHostGameMenu );
-
-
-//-----------------------------------------------------------------------------
-// Sort by player name
-//-----------------------------------------------------------------------------
-static int __cdecl PlayerNameSortFunc( vgui::ListPanel *pPanel, const vgui::ListPanelItem &item1, const vgui::ListPanelItem &item2 )
-{
-	const char *string1 = item1.kv->GetString("player");
-	const char *string2 = item2.kv->GetString("player");
-	return stricmp( string1, string2 );
+        }
+    }
 }
-
-
-//-----------------------------------------------------------------------------
-// Constructor, destructor
-//-----------------------------------------------------------------------------
-CHostGameMenu::CHostGameMenu( vgui::Panel *pParent, const char *pPanelName ) :
-	BaseClass( pParent, pPanelName )
-{
-	m_pPlayerList = new vgui::ListPanel( this, "PlayerList" );
- 	m_pPlayerList->AddColumnHeader( 0, "color", "Color", 52, 0 );
-	m_pPlayerList->AddColumnHeader( 1, "player", "Player Name", 128, 0 );
-    m_pPlayerList->SetSelectIndividualCells( false );
-	m_pPlayerList->SetEmptyListText( "No Players" );
- 	m_pPlayerList->SetDragEnabled( false );
- 	m_pPlayerList->AddActionSignalTarget( this );
-	m_pPlayerList->SetSortFunc( 0, PlayerNameSortFunc );
-	m_pPlayerList->SetSortFunc( 1, PlayerNameSortFunc );
-	m_pPlayerList->SetSortColumn( 1 );
-
-	m_pServerIP = new vgui::TextEntry( this, "ServerIP" );
-	m_pServerName = new vgui::TextEntry( this, "ServerName" );
-
-	m_pPlayerName = new vgui::TextEntry( this, "PlayerName" );
-	m_pPlayerName->SetMultiline( false );
-
-	m_pChatLog = new vgui::TextEntry( this, "ChatLog" );
-	m_pChatLog->SetMultiline( true );
-	m_pChatLog->SetVerticalScrollbar( true );
-
-	m_pChatEntry = new vgui::TextEntry( this, "ChatEntry" );
-	m_pChatEntry->AddActionSignalTarget( this );
-	m_pChatEntry->SetMultiline( false );
-    m_pChatEntry->SendNewLine( true );
-
-	m_pStartGame = new vgui::Button( this, "StartGame", "Start Game", this );
-
-	LoadControlSettings( "resource/hostgamemenu.res", "GAME" );
-
-	m_pPlayerName->SetText( "Unnamed" );
-
-	if ( !g_pNetworkManager->HostGame() )
-	{
-		m_pStartGame->SetEnabled( false );
-		return;
-	}
-
-	m_pServerIP->SetText( g_pNetworkSystem->GetLocalAddress() );
-	m_pServerName->SetText( g_pNetworkSystem->GetLocalHostName() );
-
-	g_pNetworkManager->AddListener( NETWORK_MESSAGE_SERVER_TO_CLIENT, LEGION_NETMESSAGE_GROUP, CHAT_MESSAGE, this );
-	g_pNetworkManager->AddListener( NETWORK_MESSAGE_CLIENT_TO_SERVER, LEGION_NETMESSAGE_GROUP, CHAT_MESSAGE, this );
-}
-
-CHostGameMenu::~CHostGameMenu()
-{
-	g_pNetworkManager->RemoveListener( NETWORK_MESSAGE_SERVER_TO_CLIENT, LEGION_NETMESSAGE_GROUP, CHAT_MESSAGE, this );
-	g_pNetworkManager->RemoveListener( NETWORK_MESSAGE_CLIENT_TO_SERVER, LEGION_NETMESSAGE_GROUP, CHAT_MESSAGE, this );
-}
-
-
-//-----------------------------------------------------------------------------
-// Called when a particular network message occurs
-//-----------------------------------------------------------------------------
-void CHostGameMenu::OnNetworkMessage( NetworkMessageRoute_t route, INetworkMessage *pNetworkMessage )
-{
-	if ( route == NETWORK_MESSAGE_SERVER_TO_CLIENT )
-	{
-		CNetworkMessage_Chat *pChatMsg = static_cast<CNetworkMessage_Chat*>( pNetworkMessage );
-		m_pChatLog->InsertString( pChatMsg->m_Message.Get() );
-		m_pChatLog->InsertChar( '\n' );
-	}
-	else
-	{
-		// If this message was received from an client, broadcast it to all other clients
-		g_pNetworkManager->BroadcastServerToClientMessage( pNetworkMessage );
-	}
-}
-
-
-//-----------------------------------------------------------------------------
-// Called when the enter key is hit in the chat entry window
-//-----------------------------------------------------------------------------
-void CHostGameMenu::OnTextNewLine()
-{
-	CNetworkMessage_Chat msg;
-	 
-	int nLen = m_pChatEntry->GetTextLength();
-	if ( nLen > 0 )
-	{
-		char *pText = (char*)_alloca( (nLen+1) * sizeof(char) );
-		m_pChatEntry->GetText( pText, nLen+1 );
-		m_pChatEntry->SetText( "" );
-
-		int nLenName = m_pPlayerName->GetTextLength();
-		char *pName = (char*)_alloca( (nLenName+8) * sizeof(char) );
-		if ( nLenName == 0 )
-		{
-			nLenName = 7;
-			Q_strcpy( pName, "unnamed" );
-		}
-		else
-		{
-			m_pPlayerName->GetText( pName, nLenName+1 );
-		}
-
-		int nTotalLen = nLen + nLenName;
-		msg.m_Message.SetLength( nTotalLen + 3 );
-		Q_snprintf( msg.m_Message.Get(), nTotalLen+3, "[%s] %s", pName, pText );
-
-		g_pNetworkManager->PostClientToServerMessage( &msg );
-	}
-}
-
-
-//-----------------------------------------------------------------------------
-// Called when the enter key is hit in the chat entry window
-//-----------------------------------------------------------------------------
-void CHostGameMenu::OnCommand( const char *pCommand )
-{
-	if ( !Q_stricmp( pCommand, "CancelHostGame" ) )
-	{
-		g_pNetworkManager->StopHostingGame();
-		g_pMenuManager->PopMenu();
-		return;
-	}
-
-	if ( !Q_stricmp( pCommand, "StartGame" ) )
-	{
-		IGameManager::StartNewLevel();
-		g_pMenuManager->PopAllMenus();
-		return;
-	}
-
-	BaseClass::OnCommand( pCommand );
-}
-
-
