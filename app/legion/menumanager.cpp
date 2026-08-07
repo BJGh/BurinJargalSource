d3770e9e1b9c3b0396f6e1999d96d3e303e704ba
BJ_Gh <49895341+BJGh@users.noreply.github.com>
2026-08-07 14:19:23 +0800

    Update menumanager.cpp
    Продолжим., решил так сделать. Щас вот там пару файлов осталось, затем скачаю проект. Потом удалю все файлы h консольной командой. И вообще, почему бы совсем НЕ обнаглеть? Вот у меня же есть "ТАКИЕ" инструменты. Пусть ОНО само переписывает! https://github.com/BJGh/ABSHP/tree/main/classifier_service/backbonebrains/src это внимание, J# вот пример кода "нового языка" https://github.com/BJGh/ABSHP/blob/main/classifier_service/backbonebrains/SourceFiles/Example_1.txt; идея такая... https://github.com/BJGh/ML-AntiManic-Emergency-Test/tree/main/iso_builder пусть он "прошьет" это на C# сам!

--- app/legion/menumanager.cpp
+++ app/legion/menumanager.cpp
@@ -1,195 +1,179 @@
-//========= Copyright Valve Corporation, All rights reserved. ============//
-//
-// Purpose: The menu manager  
-//
-// $Revision: $
-// $NoKeywords: $
-//===========================================================================//
+using System;
+using System.Collections.Generic;
 
-#include "menumanager.h"
-#include "vgui_controls/panel.h"
-#include "vgui_controls/frame.h"
-#include "uimanager.h"
+namespace sourcesharp.app.legion;
 
-
-//-----------------------------------------------------------------------------
-// Singleton
-//-----------------------------------------------------------------------------
-static CMenuManager s_MenuManager;
-extern CMenuManager *g_pMenuManager = &s_MenuManager;
-
-
-//-----------------------------------------------------------------------------
-// Static members.
-// NOTE: Do *not* set this to 0; it could cause us to lose some registered
-// menus since that list is set up during construction
-//-----------------------------------------------------------------------------
-IMenuFactory *CMenuManager::m_pFirstFactory;
-
-
-//-----------------------------------------------------------------------------
-// Call to register methods which can construct menus w/ particular names
-//-----------------------------------------------------------------------------
-IMenuFactory *CMenuManager::RegisterMenu( IMenuFactory *pMenuFactory )
+internal class MenuManager : IGameSystem
 {
-	// NOTE: This method is expected to be called during global constructor
-	// time, so it must not require any global constructors to be called to work
-	IMenuFactory *pPrevFactory = m_pFirstFactory;
-	m_pFirstFactory = pMenuFactory;
-	return pPrevFactory;
+    // Одиночка (Singleton)
+    private static readonly MenuManager _instance = new();
+    public static MenuManager Instance => _instance;
+
+    // Вместо кастомных фабрик Valve используем встроенные делегаты Func<Panel, Panel>
+    private readonly Dictionary<string, Func<Panel, Panel>> _menuFactories = new(StringComparer.OrdinalIgnoreCase);
+    private readonly Stack<Panel> _activeMenus = new();
+
+    // Переменные состояния фрейма
+    private bool _popRequested;
+    private bool _popAllRequested;
+    private string? _pushRequestedMenuName;
+
+    private MenuManager() { }
+
+    /// <summary>
+    /// Регистрация меню в духе вашего Terminal-подхода: связываем имя с функцией создания
+    /// </summary>
+    public void RegisterMenu(string menuName, Func<Panel, Panel> factoryFunc)
+    {
+        _menuFactories[menuName] = factoryFunc;
+    }
+
+    public bool Init()
+    {
+        // Инициализируем дефолтные меню (пример динамической регистрации вместо REGISTER_MENU макроса)
+        RegisterMenu("MainMenu", (parent) => new MainMenu(parent, "MainMenu"));
+        RegisterMenu("JoinGameMenu", (parent) => new JoinGameMenu(parent, "JoinGameMenu"));
+
+        _popRequested = false;
+        _popAllRequested = false;
+        _pushRequestedMenuName = null;
+
+        return true;
+    }
+
+    public void Shutdown()
+    {
+        CleanUpAllMenus();
+    }
+
+    public void PushMenu(string menuName)
+    {
+        if (_pushRequestedMenuName != null)
+        {
+            throw new InvalidOperationException("Менеджер меню: Нельзя запросить загрузку двух меню за один фрейм!");
+        }
+
+        if (!_menuFactories.ContainsKey(menuName))
+        {
+            Console.WriteLine($"[MenuManager] Предупреждение: Попытка загрузить неизвестное меню {menuName}");
+            return;
+        }
+
+        _pushRequestedMenuName = menuName;
+    }
+
+    public void PopMenu()
+    {
+        if (_popRequested)
+            throw new InvalidOperationException("Менеджер меню: Нельзя запросить закрытие двух меню за один фрейм!");
+        if (_pushRequestedMenuName != null)
+            throw new InvalidOperationException("Менеджер меню: Нельзя закрывать меню сразу после запроса на открытие в одном фрейме!");
+
+        _popRequested = true;
+    }
+
+    public void PopAllMenus()
+    {
+        if (_pushRequestedMenuName != null)
+            throw new InvalidOperationException("Менеджер меню: Нельзя закрывать все меню сразу после запроса на открытие в одном фрейме!");
+
+        _popAllRequested = true;
+    }
+
+    public void SwitchToMenu(string menuName)
+    {
+        if (_popRequested || _pushRequestedMenuName != null)
+            throw new InvalidOperationException("Менеджер меню: Конфликт команд переключения меню внутри одного фрейма!");
+
+        if (!_menuFactories.ContainsKey(menuName))
+        {
+            Console.WriteLine($"[MenuManager] Предупреждение: Попытка переключения на неизвестное меню {menuName}");
+            return;
+        }
+
+        _popRequested = true;
+        _pushRequestedMenuName = menuName;
+    }
+
+    public string? GetTopmostPanelName()
+    {
+        return _activeMenus.Count == 0 ? null : _activeMenus.Peek().Name;
+    }
+
+    /// <summary>
+    /// Пофреймовое обновление состояний окон интерфейса
+    /// </summary>
+    public void Update()
+    {
+        if (_popAllRequested)
+        {
+            CleanUpAllMenus();
+            _popAllRequested = false;
+            return;
+        }
+
+        // Обработка закрытия (Pop)
+        if (_popRequested)
+        {
+            if (_activeMenus.Count == 0)
+                throw new InvalidOperationException("Менеджер меню: Попытка закрыть меню, когда стек пуст!");
+
+            Panel topMenu = _activeMenus.Pop();
+            topMenu.Dispose(); // Вместо MarkForDeletion() в C# используем стандартный Dispose
+
+            if (_activeMenus.Count > 0)
+            {
+                Panel newTop = _activeMenus.Peek();
+                newTop.SetVisible(true);
+                newTop.SetParent(UIManager.Instance.GetRootPanel(UserInterfaceRoot.Menu));
+            }
+            else
+            {
+                UIManager.Instance.EnablePanel(UserInterfaceRoot.Menu, false);
+            }
+            _popRequested = false;
+        }
+
+        // Обработка открытия (Push)
+        if (_pushRequestedMenuName != null)
+        {
+            if (_activeMenus.Count > 0)
+            {
+                Panel previousTop = _activeMenus.Peek();
+                previousTop.SetVisible(false);
+                previousTop.SetParent(null);
+            }
+            else
+            {
+                UIManager.Instance.EnablePanel(UserInterfaceRoot.Menu, true);
+            }
+
+            // Вызываем делегат-фабрику напрямую по имени
+            Panel rootPanel = UIManager.Instance.GetRootPanel(UserInterfaceRoot.Menu);
+            Panel newMenu = _menuFactories[_pushRequestedMenuName](rootPanel);
+            
+            _activeMenus.Push(newMenu);
+            
+            if (newMenu is Frame frame)
+            {
+                frame.Activate(); // Активируем окно, если это Frame
+            }
+
+            _pushRequestedMenuName = null;
+        }
+    }
+
+    public void CleanUpAllMenus()
+    {
+        while (_activeMenus.Count > 0)
+        {
+            Panel topMenu = _activeMenus.Pop();
+            topMenu.Dispose();
+        }
+        UIManager.Instance.EnablePanel(UserInterfaceRoot.Menu, false);
+    }
 }
 
-	
-//-----------------------------------------------------------------------------
-// Init, shutdown
-//-----------------------------------------------------------------------------
-bool CMenuManager::Init()
-{
-	// Build a dictionary of all registered menus
-	IMenuFactory *pFactory;
-	for ( pFactory = m_pFirstFactory; pFactory; pFactory = pFactory->GetNextFactory() )
-	{
-		m_MenuFactories.Insert( pFactory->GetMenuName(), pFactory );
-	}
-
-	m_bPopRequested = false;
-	m_bPopAllRequested = false;
-	m_pPushRequested = NULL;
-
-	return true;
-}
-
-void CMenuManager::Shutdown()
-{
-	CleanUpAllMenus();
-}
-
-	
-//-----------------------------------------------------------------------------
-// Push, pop menus
-//-----------------------------------------------------------------------------
-void CMenuManager::PushMenu( const char *pMenuName )
-{
-	AssertMsg( !m_pPushRequested, "Can't request to push two menus in a single frame!" );
-
-	MenuFactoryIndex_t i = m_MenuFactories.Find( pMenuName );
-	if ( i == m_MenuFactories.InvalidIndex() )
-	{
-		Warning( "Tried to push unknown menu %s\n", pMenuName );
-		return;
-	}
-	m_pPushRequested = m_MenuFactories[i];
-}
-
-void CMenuManager::PopMenu( )
-{
-	AssertMsg( !m_bPopRequested, "Can't request to pop two menus in a single frame!" );
-	AssertMsg( !m_pPushRequested, "Can't request to pop after requesting to push a menu in a single frame!" );
-	m_bPopRequested = true;
-}
-
-void CMenuManager::PopAllMenus( )
-{
-	AssertMsg( !m_pPushRequested, "Can't request to pop after requesting to push a menu in a single frame!" );
-	m_bPopAllRequested = true;
-}
-
-
-//-----------------------------------------------------------------------------
-// Request a menu to switch to
-//-----------------------------------------------------------------------------
-void CMenuManager::SwitchToMenu( const char *pMenuName )
-{
-	AssertMsg( !m_bPopRequested, "Can't request to pop two menus in a single frame!" );
-	AssertMsg( !m_pPushRequested, "Can't request to push two menus in a single frame!" );
-
-	MenuFactoryIndex_t i = m_MenuFactories.Find( pMenuName );
-	if ( i == m_MenuFactories.InvalidIndex() )
-	{
-		Warning( "Tried to switch to unknown menu %s\n", pMenuName );
-		return;
-	}
-	m_bPopRequested = true;
-	m_pPushRequested = m_MenuFactories[i];
-}
-
-
-//-----------------------------------------------------------------------------
-// Returns the name of the topmost panel
-//-----------------------------------------------------------------------------
-const char *CMenuManager::GetTopmostPanelName()
-{
-	if ( !m_nActiveMenu.Count() )
-		return NULL;
-	return m_nActiveMenu.Top()->GetName();
-}
-
-	
-//-----------------------------------------------------------------------------
-// Request a menu to switch to
-//-----------------------------------------------------------------------------
-void CMenuManager::Update( )
-{
-	if ( m_bPopAllRequested )
-	{
-		CleanUpAllMenus();
-		m_bPopAllRequested = false;
-		return;
-	}
-
-	if ( m_bPopRequested )
-	{
-		AssertMsg( m_nActiveMenu.Count(), "Tried to pop a menu when no menus are active" );
-		vgui::Panel *pTop = m_nActiveMenu.Top();
-		pTop->MarkForDeletion();
-		m_nActiveMenu.Pop();
-
-		// Mark the new active menu as visible, attach it to hierarchy.
-		if ( m_nActiveMenu.Count() > 0 )
-		{
-			vgui::Panel *pTop = m_nActiveMenu.Top();
-			pTop->SetVisible( true );
-			pTop->SetParent( g_pUIManager->GetRootPanel( UI_ROOT_MENU ) );
-		}
-		else
-		{
-			g_pUIManager->EnablePanel( UI_ROOT_MENU, false );
-		}
-		m_bPopRequested = false;
-	}
-
-	if ( m_pPushRequested )
-	{
-		// Mark the previous menu as not visible, detach it from hierarchy.
-		if ( m_nActiveMenu.Count() > 0 )
-		{
-			vgui::Panel *pTop = m_nActiveMenu.Top();
-			pTop->SetVisible( false );
-			pTop->SetParent( (vgui::Panel*)NULL );
-		}
-		else
-		{
-			g_pUIManager->EnablePanel( UI_ROOT_MENU, true );
-		}
-		vgui::Panel *pMenu = m_pPushRequested->CreateMenu( g_pUIManager->GetRootPanel( UI_ROOT_MENU ) );
-		m_nActiveMenu.Push( pMenu );
-		static_cast<vgui::Frame*>( pMenu )->Activate();
-		m_pPushRequested = NULL;
-	}
-}
-
-	
-//-----------------------------------------------------------------------------
-// Cleans up all menus
-//-----------------------------------------------------------------------------
-void CMenuManager::CleanUpAllMenus()
-{
-	while ( m_nActiveMenu.Count() )
-	{
-		vgui::Panel *pTop = m_nActiveMenu.Top();
-		pTop->MarkForDeletion();
-		m_nActiveMenu.Pop();
-	}
-	g_pUIManager->EnablePanel( UI_ROOT_MENU, false );
-}
-
+// --- Заглушки типов для корректной сборки ---
+internal enum UserInterfaceRoot { Menu }
+internal class Frame : Panel { public Frame(Panel p, string n) : base(p, n) { } public void Activate() { } }
